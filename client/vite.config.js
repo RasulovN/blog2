236e657cb8fd7c7f6a2e905a398221589a150e1b
Nurbek <92941063+RasulovN@users.noreply.github.com>
2024-05-16 11:16:37 +0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://blog2-4f6h7azha-nurbekrasulov71gmailcoms-projects.vercel.app/',
         secure: false,
       },
     },
